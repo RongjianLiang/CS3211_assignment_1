bf86b7e41fc60cb177f8f1e52d9a7f60167977de
RongjianLiang <rongjianliang67@gmail.com>
2023-02-22 17:53:31 +0800

    add floor division for timestamp

--- engine.hpp
+++ engine.hpp
@@ -7,6 +7,7 @@
 #include <chrono>
 #include <iostream>
 #include <cstring>
+#include <cmath>
 #include <vector>
 #include <algorithm>
 #include <string>
@@ -17,7 +18,8 @@
 // a timstamping scheme should be implemented here?
 inline std::chrono::microseconds::rep getCurrentTimestamp() noexcept
 {
-	return std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
+	return std::floor(std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now().time_since_epoch()).count()
+	/ 1000000);
 }
 
 struct Engine
