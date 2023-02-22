b52ac596b331c168caeba0d81fe14d5e32413a19
RongjianLiang <rongjianliang67@gmail.com>
2023-02-22 17:04:23 +0800

    debug

--- OrderBook.cpp
+++ OrderBook.cpp
@@ -92,9 +92,9 @@ public:
 		for(auto it = this->books.begin(); it != this->books.end(); it++){
 			if((*it).order_id == input.order_id){
 				// compare the timestamp and check if the order is fully filled 
-				// std::cout << "timestamp check: "<< ((*it).time_stamp < time_stamp) <<std::endl;
-				// std::cout << "count check: "<< ((*it).count > 0) << std::endl;
-				// result = (((*it).time_stamp < time_stamp) && ((*it).count > 0)); 
+				std::cout << "timestamp check: "<< ((*it).time_stamp < time_stamp) <<std::endl;
+				std::cout << "count check: "<< ((*it).count > 0) << std::endl;
+				result = (((*it).time_stamp < time_stamp) && ((*it).count > 0)); 
 				std::cout << "result: " << result << std::endl;
 				break;
 			}
