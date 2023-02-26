adc6f709ea28e4269eee02a94975a88f94872060
parth-io <67057645+parth-io@users.noreply.github.com>
2023-02-26 09:40:47 +0800

    Update test runner

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -7,27 +7,8 @@ pushd .
 cd ..
 make
 popd
-if [[ "$1" == "all" ]]; then
-  for filename in ${arr[*]}; do
-    BASE_FILENAME=$(basename "$filename" .in)
-    CONTAINING_DIRECTORY=$(dirname "$filename")
-    # read -p "Run $filename? (Y/N): " confirm
-    #if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
-    mkdir -p "$BASE_FILENAME"
-    cd "./${BASE_FILENAME}"
-    ../../grader-linux ../../engine < "../${filename}" > "${BASE_FILENAME}.log" 2>&1
-  
-    TAIL=$(tail -n 1 "${BASE_FILENAME}.log")
-    if [[ $TAIL == "test passed" ]]; then
-        echo "Passed $filename"
-    else
-        echo
-        echo "***Failed to run $filename***"
-    fi
-    cd ..
-    #fi
-  done
-else
+
+run_individual_test () {
   BASE_FILENAME=$(basename "$1" .in)
   echo $BASE_FILENAME
   CONTAINING_DIRECTORY=$(dirname "$1")
@@ -43,8 +24,19 @@ else
         echo "***Failed to run $1*** for $i"
     fi
   done
-  
   cd ../..
+}
+
+    # read -p "Run $filename? (Y/N): " confirm
+    #if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+
+
+if [[ "$1" == "all" ]]; then
+  for filename in ${arr[*]}; do
+    run_individual_test $filename
+  done
+else
+  run_individual_test $1
 fi
 unset IFS
 
